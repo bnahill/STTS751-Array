85e7cfd079425495912fd28e9db44968e3b8155a
Ben Nahill <bnahill@gmail.com>
2012-04-06 16:47:00 -0400

    New startup for GNU toolchain since FPU config is now handled elsewhere

--- sw/src/system/startup_stm32f4xx.s
+++ sw/src/system/startup_stm32f4xx.s
@@ -93,11 +93,13 @@ LoopFillZerobss:
   cmp  r2, r3
   bcc  FillZerobss
 
-/* FPU settings */
- ldr     r0, =0xE000ED88           /* Enable CP10,CP11 */
+/* FPU settings (handled in system_stm32....) */
+/*
+ ldr     r0, =0xE000ED88
  ldr     r1,[r0]
  orr     r1,r1,#(0xF << 20)
  str     r1,[r0]
+*/
 /* Call the clock system intitialization function.*/
   bl  SystemInit   
 /* Call the application's entry point.*/
